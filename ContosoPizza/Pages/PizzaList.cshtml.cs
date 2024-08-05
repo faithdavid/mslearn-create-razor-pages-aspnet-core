770b524ad49eddd9e6dc92af46b27bc54096ead2
faithdavid <59588415+faithdavid@users.noreply.github.com>
2024-08-05 13:29:55 +0000

    learning and added new page PizzaList.cshtml

---
+++ ContosoPizza/Pages/PizzaList.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace ContosoPizza.Pages
+{
+    public class PizzzaListModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
